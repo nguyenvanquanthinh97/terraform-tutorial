7274d18a5bc4d7855763c72c39c7f49218d23a74
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2024-03-08 07:59:08 +0700

    security group in aws

---
+++ firewall.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+	region = "ap-southeast-1"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-firewalls"
+  description = "Managed from Terraformc"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 100
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
