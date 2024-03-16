e37bb18cfb5e68b568cdafd0d1488a568235290a
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2024-03-16 23:03:38 +0700

    saving terraform plan to file

---
+++ README.md
@@ -0,0 +1,2 @@
+Use the command `terraform plan -out=plan-file.txt` to save plan to that file
+Use the command `terraform apply plan-file.txt` to apply with saved plan
---
+++ plan-file.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "ap-southeast-1"
+}
+
+data "aws_ami" "app_ami" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+      name = "name"
+      values = ["amzn2-ami-hvm*"]
+    }
+}
+
+resource "aws_instance" "myec2" {
+    ami = data.aws_ami.app_ami.id
+    instance_type = "t2.large"
+}
