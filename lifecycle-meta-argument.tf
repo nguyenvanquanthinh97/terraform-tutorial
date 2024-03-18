4e6f13c58816d79a9464246502db564b1d51daf1
thinhvqnguyen <thinh.q.nguyen@opswat.com>
2024-03-18 11:27:28 +0700

    lifecycle meta argument

---
+++ lifecycle-meta-argument.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-09b1e8fc6368b8a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
