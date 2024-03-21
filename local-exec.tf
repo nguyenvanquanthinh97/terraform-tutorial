ea514527796633f12e005f20532ed8f04dfa4d60
thinhvqnguyen <thinh.q.nguyen@opswat.com>
2024-03-21 11:24:57 +0700

    provisioner types

--- local-exec.tf
+++ local-exec.tf
@@ -1,9 +1,9 @@
 
 resource "aws_instance" "web" {
-    ami = data.aws_ami.app_ami.id
-    instance_type = "t2.micro"
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
 
-    provisioner "local-exec" {
-        command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
-    }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.web.private_ip} >> private_ips.txt"
+  }
 }
---
+++ provisioner-types.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH into VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Outbound Allowed"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "mynano" {
+    ami = data.aws_ami.app_ami.id
+    instance_type = "t2.micro"
+    key_name = "terraform"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum -y install nano"
+        ]
+    }
+
+    provisioner "remote-exec" {
+        when = destroy
+        inline = [
+            "sudo yum -y remove nano"
+        ]
+    }
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        private_key = file("./terraform.pem")
+        host = self.public_ip
+    }
+}
