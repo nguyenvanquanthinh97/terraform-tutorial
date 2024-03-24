5c601d45af5f6758a3abc73b5d4df6460c612ad4
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2024-03-24 16:29:40 +0700

    terraform workspace default/dev/prod

---
+++ kplabs-workspace.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = lookup(var.instance_type, terraform.workspace)
+}
+
+variable "instance_type" {
+  type = map(string)
+
+  default = {
+    default = "t2.nano"
+    dev     = "t2.micro"
+    prod    = "t2.large"
+  }
+}
